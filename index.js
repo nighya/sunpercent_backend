02e8937c91671424450e18c4637fb0c848332bb2
rjatkdudgh@naver.com <rjatkdudgh@naver.com>
2022-03-07 11:28:26 +0900

    https 적용

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require("express");
+const app = express();
 const fs = require("fs");
 const cors = require("cors");
 const path = require("path");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 
-const app = express();
+const https = require("https");
+const server = https.createServer(
+  {
+    key: fs.readFileSync("./cert/domain.com.key"),
+    cert: fs.readFileSync("./cert/domain.com.crt"),
+    ca: fs.readFileSync("./cert/rootca.crt"),
+    requestCert: false,
+    rejectUnauthorized: false,
+  },
+  app
+);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", express.static(__dirname + "/public"));
-app.use(cors({ origin: "http://192.168.0.12:8080", credentials: true }));
+app.use(cors({ origin: "https://192.168.0.12:8080", credentials: true }));
 
 const port = 4000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("server listening on port", port);
 });
 
