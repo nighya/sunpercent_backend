168cc7ee361ef34fd86c96a55558ba89ba0b9a65
rjatkdudgh@naver.com <rjatkdudgh@naver.com>
2022-03-07 13:12:18 +0900

    peerjs nodejs적용후 vue 에서 실행하기 시작 httponly secure 적용 텝이 초기단계

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const cors = require("cors");
-const path = require("path");
+// const path = require("path");
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt-nodejs");
+// const bcrypt = require("bcrypt-nodejs");
 
 const https = require("https");
 const server = https.createServer(
@@ -24,9 +24,40 @@ app.use(express.json());
 app.use("/", express.static(__dirname + "/public"));
 app.use(cors({ origin: "https://192.168.0.12:8080", credentials: true }));
 
+
+const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+
+    socket.to(roomId).on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
+
 const port = 4000;
 server.listen(port, () => {
-  console.log("server listening on port", port);
+  console.log("server listening on port", port,`https://192.168.0.12:${port}`);
 });
 
 //가져오기
