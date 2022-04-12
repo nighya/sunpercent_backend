10cf8f92ff31891514dca4034542334088eecc7d
rjatkdudgh@naver.com <rjatkdudgh@naver.com>
2022-04-13 05:39:42 +0900

    화상채팅 삭제

--- index.js
+++ index.js
@@ -24,36 +24,7 @@ app.use(express.json());
 app.use("/", express.static(__dirname + "/public"));
 app.use(cors({ origin: "https://192.168.0.12:8080", credentials: true }));
 
-
-const io = require("socket.io")(server);
-const { v4: uuidV4 } = require("uuid");
-
-app.set("view engine", "ejs");
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.redirect(`/${uuidV4()}`);
-});
-
-app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
-});
-
-io.on("connection", (socket) => {
-  socket.on("join-room", (roomId, userId) => {
-    socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connected", userId);
-
-    socket.to(roomId).on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
-
-
-    socket.on("disconnect", () => {
-      socket.to(roomId).broadcast.emit("user-disconnected", userId);
-    });
-  });
-});
 
 const port = 4000;
 server.listen(port, () => {
