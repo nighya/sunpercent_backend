c2e96dd536334cfde438c3371943f3889832b202
rjatkdudgh@naver.com <rjatkdudgh@naver.com>
2022-07-13 11:39:01 +0900

    ing

--- controllers/noteCtrl.js
+++ controllers/noteCtrl.js
@@ -24,40 +24,58 @@ const noteCtrl = {
     ];
     const sql =
       "INSERT INTO note(to_uid, from_uid, to_nickname,from_nickname, title, message, from_gender,date) values(?,?,?,?,?,?,?,?)";
-    const confirm_sql = "SELECT user_uid FROM members WHERE  user_uid  LIKE ? AND nickname LIKE ?";
-   
+
+    const to_uid_sql = "SELECT user_uid FROM members WHERE nickname=?";
+
+    const confirm_sql =
+      "SELECT user_uid FROM members WHERE  user_uid  LIKE ? AND nickname LIKE ?";
+
     const cookie = req.headers.cookie;
     const token = cookie.replace("HrefreshToken=", "");
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     if (decoded) {
       var base64Payload = token.split(".")[1];
       var payload = Buffer.from(base64Payload, "base64");
       var result = JSON.parse(payload.toString());
-      if (from_uid === result.user_uid) {
-        connection.query(confirm_sql, [to_uid,to_nickname], (err, data) => {
-          if (data.length > 0) {
-            try {
-              connection.query(sql, datas, (err_1, row_1) => {
-                if (err_1) {
-                  console.log("note 에러  :   " + err_1);
+      console.log(to_uid);
+      connection.query(to_uid_sql, to_nickname, (err_1, data_1) => {
+        console.log("data_1  :  " + Object.values(data_1[0]));
+        if (data_1.length > 0) {
+          to_uid = Object.values(data_1[0]);
+          console.log(datas.to_uid)
+          console.log(datas)
+          if (from_uid === result.user_uid) {
+            connection.query(
+              confirm_sql,
+              [to_uid, to_nickname],
+              (err, data_2) => {
+                if (data_2.length > 0) {
+                  try {
+                    connection.query(sql, datas, (err_2, row_2) => {
+                      if (err_2) {
+                        console.log("note 에러  :   " + err_2);
+                      } else {
+                        res.sendStatus(200);
+                      }
+                    });
+                  } catch (err_c) {
+                    console.log("note catch 에러  :   " + err_c);
+                  }
                 } else {
-                  res.sendStatus(200);
+                  res.sendStatus(400);
                 }
-              });
-            } catch (err_c) {
-              console.log("note catch 에러  :   " + err_c);
-            }
+              }
+            );
           } else {
-            res.sendStatus(400);
+            res.status(403).json({
+              message: "fobbiden",
+            });
+            // res.sendStatus(403)
           }
-        });
-      } else {
-        res.status(403).json({
-          message: "fobbiden",
-        });
-        // res.sendStatus(403)
-      }
+        } else {
+          res.sendStatus(400);
+        }
+      });
     }
   },
   getSentNote: async (req, res) => {
@@ -82,7 +100,7 @@ const noteCtrl = {
           (err, data) => {
             if (data.length > 0) {
               try {
-                console.log("sentnote data 보냄")
+                console.log("sentnote data 보냄");
                 res.send(data);
               } catch (err_c) {
                 console.log("getsentnote catch 에러  :   " + err_c);
@@ -122,7 +140,7 @@ const noteCtrl = {
           (err, data) => {
             if (data.length > 0) {
               try {
-                console.log("receivednote data 보냄")
+                console.log("receivednote data 보냄");
                 res.send(data);
               } catch (err_c) {
                 console.log("receivednote catch 에러  :   " + err_c);
