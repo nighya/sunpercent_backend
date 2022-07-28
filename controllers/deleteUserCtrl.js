a11ec241099169eca6c55033bad9f456246ec208
rjatkdudgh@naver.com <rjatkdudgh@naver.com>
2022-07-28 14:15:59 +0900

    ing

--- controllers/deleteUserCtrl.js
+++ controllers/deleteUserCtrl.js
@@ -14,18 +14,13 @@ const deleteUserCtrl = {
     const nickname = req.body.nickname;
     const password = req.body.password;
 
-    const sql_params = [user_uid, email, nickname, password];
-
     const login_sql = "SELECT * FROM members WHERE email=?";
-
     const select_profile_image_path_sql =
       "SELECT profile_image FROM sunpercent.members WHERE user_uid  LIKE ? AND email LIKE ? AND nickname LIKE ?";
     const deleteProfileImage_sql =
       "UPDATE sunpercent.members SET profile_image=? WHERE user_uid =?";
-
     const select_image_path_sql =
       "SELECT image_path FROM sunpercent.images WHERE user_uid LIKE ? AND nickname LIKE ?";
-
     const delete_user_sql =
       "DELETE FROM sunpercent.members WHERE user_uid  LIKE ? AND email LIKE ? AND nickname LIKE ?";
     const delete_score_sql = "DELETE FROM sunpercent.score WHERE to_uid=?";
@@ -41,8 +36,8 @@ const deleteUserCtrl = {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     //login 확인
-    connection.query(login_sql, email, (err_0, row_0) => {
-      if (err_0)
+    connection.query(login_sql, email, async (err_0, row_0) => {
+      if (err_0) {
         return res.status(400).json({
           error: [
             {
@@ -50,8 +45,8 @@ const deleteUserCtrl = {
             },
           ],
         });
-      if (row_0.length > 0) {
-        bcrypt.compare(password, row_0[0].password, (err_01, result) => {
+      } else if (row_0.length > 0) {
+        bcrypt.compare(password, row_0[0].password, async (err_01, result) => {
           if (result) {
             if (decoded) {
               var base64Payload = token.split(".")[1];
@@ -61,108 +56,115 @@ const deleteUserCtrl = {
               if (user_uid === result.user_uid) {
                 try {
                   //profile image 지우기
-                  connection.query(
+                  await connection.query(
                     select_profile_image_path_sql,
                     [user_uid, email, nickname],
-                    (err_1, row_1) => {
+                     (err_1, row_1) => {
                       //   console.log("row_1 랭스 : " + JSON.stringify(row_1));
                       //   console.log("row_2 랭스 : " + Object.values(row_1[0]));
-                      try {
-                        if (Object.values(row_1[0]) != "") {
-                          console.log(
-                            "fs  :  " + Object.values(row_1[0]) + ":"
-                          );
-                          fs.unlinkSync(`public${Object.values(row_1[0])}`);
-                          connection.query(
-                            deleteProfileImage_sql,
-                            [null, user_uid],
-                            (err_02, row_02) => {
-                              if (err_02) {
-                                console.log("err_02 에러 : " + err_02);
-                              }
-                            }
-                          );
-                        } //image들 지우기
+                      console.log("log_1");
+                      if (Object.values(row_1[0]) != "") {
+                        fs.unlinkSync(`public${Object.values(row_1[0])}`);
                         connection.query(
-                          select_image_path_sql,
-                          [user_uid, nickname],
-                          (err_2, row_2) => {
-                            if (err_2) {
-                              console.log("err_2 에러  : " + err_2);
-                            }
-                            if (row_2.length > 0) {
-                              try {
-                                row_2.map((data) => {
-                                  console.log("data:  " + Object.values(data));
-                                  fs.unlinkSync(`public${Object.values(data)}`);
-                                });
-                                //content 지우기
-                                connection.query(
-                                  delete_content_sql,
-                                  [user_uid, nickname],
-                                  (err_3, row_3) => {
-                                    if (err_3) {
-                                      console.log("err_3 에러  : " + err_3);
-                                    }
-                                  }
-                                );
-                                //score 지우기
-                                connection.query(
-                                  delete_score_sql,
-                                  user_uid,
-                                  (err_4, row_4) => {
-                                    if (err_4) {
-                                      console.log("err_4 에러  : " + err_4);
-                                    }
-                                  }
-                                );
-                                //note 지우기
-                                connection.query(
-                                  delete_note_sql,
-                                  [user_uid, nickname],
-                                  (err_4_1, row_4_1) => {
-                                    if (err_4_1) {
-                                      console.log("err_4_1 에러  : " + err_4_1);
-                                    }
-                                  }
-                                );
-                                //user 지우기
-                                connection.query(
-                                  delete_user_sql,
-                                  [user_uid, email, nickname],
-                                  (err_5, row_5) => {
-                                    if (err_5) {
-                                      console.log("err_5 에러  : " + err_5);
-                                    }
-                                  }
-                                );
-                                connection.query(
-                                  insert_deleted_user_sql,
-                                  [email, deleted_user_date],
-                                  (err_6, row_6) => {
-                                    if (err_6) {
-                                      console.log("err_5 에러  : " + err_6);
-                                      res.sendStatus(400);
-                                    } else {
-                                      console.log(
-                                        "user 지우고 지우기로그 성공  : " +
-                                          JSON.stringify(row_6)
-                                      );
-                                      res.sendStatus(200);
-                                    }
-                                  }
-                                );
-                              } catch (err) {}
+                          deleteProfileImage_sql,
+                          [null, user_uid],
+                          (err_02, row_02) => {
+                            if (err_02) {
+                              console.log("err_02 에러 : " + err_02);
                             }
                           }
                         );
-                      } catch (err) {
-                        console.log("err catch 에러  : " + err);
+                      } else {
+                        console.log("else 1");
+                      }
+                    }
+                  );
+                  //image들 지우기
+                  await  connection.query(
+                    select_image_path_sql,
+                    [user_uid, nickname],
+                    (err_2, row_2) => {
+                      if (err_2) {
+                        console.log("err_2 에러  : " + err_2);
+                      }
+                      else if (row_2[0] != null) {
+                        console.log("log_2  : " + row_2[0]);
+                        row_2.map((data) => {
+                          console.log("data:  " + Object.values(data));
+                          fs.unlinkSync(`public${Object.values(data)}`);
+                        });
+                      } else {
+                        console.log("else 2")
+                      }
+                    }
+                  );
+                  //content 지우기
+                  await  connection.query(
+                    delete_content_sql,
+                    [user_uid, nickname],
+                    (err_3, row_3) => {
+                      if (err_3) {
+                        console.log("err_3 에러  : " + err_3);
+                      } else {
+                        console.log("else 3")
+                      }
+                    }
+                  );
+                  //score 지우기
+                  await   connection.query(
+                    delete_score_sql,
+                    user_uid,
+                    (err_4, row_4) => {
+                      if (err_4) {
+                        console.log("err_4 에러  : " + err_4);
+                      } else {
+                        console.log("else 4")
+                      }
+                    }
+                  );
+                  //note 지우기
+                  await   connection.query(
+                    delete_note_sql,
+                    [user_uid, nickname],
+                    (err_4_1, row_4_1) => {
+                      if (err_4_1) {
+                        console.log("err_4_1 에러  : " + err_4_1);
+                      } else {
+                        console.log("else 5")
+                      }
+                    }
+                  );
+                  //user 지우기
+                  await  connection.query(
+                    delete_user_sql,
+                    [user_uid, email, nickname],
+                    (err_5, row_5) => {
+                      if (err_5) {
+                        console.log("err_5 에러  : " + err_5);
+                      } else {
+                        console.log("else 6")
+                      }
+                    }
+                  );
+                  //지우고 로그
+                  await  connection.query(
+                    insert_deleted_user_sql,
+                    [email, deleted_user_date],
+                    (err_6, row_6) => {
+                      if (err_6) {
+                        console.log("err_5 에러  : " + err_6);
+                        res.sendStatus(400);
+                      } else {
+                        console.log(
+                          "user 지우고 지우기로그 성공  : " +
+                            JSON.stringify(row_6)
+                        );
+                        res.sendStatus(200);
                       }
                     }
                   );
                 } catch (err) {
-                  console.log("main catch 에러");
+                  console.log("catch_1 에러");
                 }
               } else {
                 res.status(403).json({
